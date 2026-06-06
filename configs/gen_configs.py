021412f8ecb011b54357ae9aba5f93c3e0594576
wizjany <wizjany@gmail.com>
2026-06-06 13:16:50 -0400

    Better config gen + compose gen, add docker build workflow.

--- configs/gen_configs.py
+++ configs/gen_configs.py
@@ -3,11 +3,11 @@ import sys
 
 region_groups = {
     # main regions
-    3: [17, 18, 19],
-    2: [12, 13, 14],
-    1: [7, 8, 9],
+    (3, "top"): [17, 18, 19],
+    (2, "mid"): [12, 13, 14],
+    (1, "bot"): [7, 8, 9],
     # temp regions
-    4: [3, 11, 15, 23]
+    (4, "temp"): [3, 11, 15, 23]
 }
 
 # extra dump config for global-mirrored state which we only want one copy of
@@ -16,22 +16,39 @@ global_dumps = 13
 out_dir = sys.argv[1] if len(sys.argv) > 1 else ""
 
 with open("prism.toml.template") as f:
-    template = f.read()
+    prism_template = f.read()
 
 with open("global_dumps.toml.template") as f:
     global_template = f.read()
 
-before, after = template.split("#__REGIONS__#")
+with open("compose.yml.template") as f:
+    compose_template = f.read()
 
-for group, regions in region_groups.items():
+compose_header, compose_service = compose_template.split("$SERVICE$", 1)
+
+before, after = prism_template.split("$REGIONS$")
+
+offset1 = 0
+offset2 = 15
+
+compose_output = compose_header
+
+for (group, name), regions in region_groups.items():
+    compose_output += compose_service.replace("$GROUP$", str(group)).replace("$NAME$", "prism-" + (name or str(group)))
+
     outfile = f"prism-{group}.toml"
-    output = "" + before
+    output = "" + before.replace("$OFFSET$", str(offset1)).replace("$OFFSET2$", str(offset2))
     for region in regions:
         output += "\n[[upstream.regions]]\n"\
                   f"name = \"bitcraft-live-{region}\"\n"\
                   f"id = {region}\n"
         if region == global_dumps:
-            output += global_template
+            output += global_template.replace("$OFFSET$", str(offset1)).replace("$OFFSET2$", str(offset2))
     output += after
+    offset1 += 1
+    offset2 += 1
     with open(os.path.join(out_dir, outfile), "w") as f:
         f.write(output)
+
+with open(os.path.join(out_dir, "docker-compose.prism.yml"), "w") as f:
+    f.write(compose_output)
